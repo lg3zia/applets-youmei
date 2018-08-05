b1ac04c68de714185c8a1f4d5d11c06fe5518b4a
luogang2 <luogang2@motorola.com>
2018-08-05 13:43:00 +0800

    adjust home page css

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,19 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-    indicatorDots: true,
-    autoplay: false,
-    interval: 100,
-    duration: 3000,
-    currentIndex: 0,
-    imgUrls: [
+    swiperIndicatorDots: true,
+    swiperAutoplay: false,
+    swiperInterval: 100,
+    swiperDuration: 2000,
+    swiperCurrentIndex: 0,
+    swiperImgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
     img_hot: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533233681024&di=a01631dbd72137ac84afad4b6c2439f9&imgtype=0&src=http%3A%2F%2Fwww.qncye.com%2Fhtml%2Fimages%2Fdianzhi.jpg',
-    contact_number: 19859180244,
-    contact_address: "常州CBD",
+    contactNumber: 19859180244,
+    contactAddress: "常州CBD",
+    listImgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ], 
   },
 
   /**
@@ -76,6 +81,18 @@ Page({
 
   },
 
+  swiperChanged: function(e) { //轮播图发生改变
+    this.setData({
+      swiperCurrentIndex: e.detail.current
+    })
+  },
+
+  swiperItemClick: function(e) {
+    wx.showToast({
+      title: this.data.swiperCurrentIndex + "",
+    })
+  },
+
   goCompanyMap: function(e) {
     wx.openLocation({
       latitude: 31.841076, // 纬度，范围为-90~90，负数表示南纬
@@ -83,20 +100,16 @@ Page({
       scale: 28, // 缩放比例          
     })
   },
-  callme: function(e) {
+
+  callMe: function(e) {
     wx.makePhoneCall({
       phoneNumber: '19859180244' //仅为示例，并非真实的电话号码
     })
   },
-  swiperChanged: function (e) {//轮播图发生改变
-    this.setData({
-      currentIndex: e.detail.current
-    })
-  },
-  swiperItemClick: function (e) {
-    wx.showToast({
-      title: this.data.currentIndex + "",
-    })
-  },
 
+  productItemClick: function(e) {
+    wx.showToast({
+      title: e.currentTarget.dataset.index + ""
+    })
+  },
 })
