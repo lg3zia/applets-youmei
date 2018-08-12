4178c57de9a8fa5ee2a1f6f0fe58a232fbf4eeb5
luogang2 <luogang2@motorola.com>
2018-08-12 13:11:59 +0800

    complete company

--- pages/news/news_item.js
+++ pages/news/news_item.js
@@ -8,35 +8,20 @@ Page({
     listNews: [{
       id: 0,
       imgPath: 'https://thumbnail0.baidupcs.com/thumbnail/e00f6f174f69b5618e20a45397b08b0f?fid=1913150091-250528-634709536241375&time=1533459600&rt=sh&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-RurU%2BLcNf22Cah47VT8cvf5bPQU%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=5026409622566650616&dp-callid=0&size=c710_u400&quality=100&vuk=-&ft=video',
-      title: "冰瓷牙",
+      title: "价格三千和价格一万的牙齿矫正区别在哪？",
       summary: "冰瓷牙采用一对一私人订制，颠覆传统美牙工艺",
       detail: "网友真事：朋友在普通诊所矫正门牙缝隙耗时2年，三年后又恢复了原样。我矫正过两次，很想说一下。我初中时矫正第一次，在镇上的一个医院(还某某区第二人民医院) ，1000多那种，当时就是牙齿外凸，矫正好稍有好转，但是没有让我完全满意，爸爸说什么都不能做到完美的呀，我就也没有怎么在意了。上大学后室友很惊讶我竟然是矫正过牙齿的！上大学后，因为有口腔课，老师越上课，我越觉得自己牙齿丑，而且侧面看嘴是凸的。于是去找老师再次矫正了。是我们学校的附属医院，正常费用是1万多到3万多，我找老师拿的友情价(具体是多少就不说了) 。第一次矫正时是没有拔牙的，这次拔了八颗。之后会打支抗钉，我相信这次一定会很有效果。其实矫正牙齿的材料真的是不贵，完全是靠医生的技术，以前那个医生竟然没有给我拔牙？把我牙齿硬往里挤？上过口腔课之后我真的好怀疑那医生的技术。为什么正畸费用差异这么大呢？其实很重要的原因是矫正牙齿是个性化的医疗服务，并不是可以简单复制、衡量的『商品』。所以，无法根据结果给出一个评判进而去定价。举个容易理解的例子，有点像学生的补习班，每个学生基础不一样，对于20分的学生能提高到60分，那么这个培训班就是超值；但对于80分提高到85分，可能上不上这个培训班就需要商榷了。正畸治疗并没有一个严格的标准，所以，更不能用一个结束的标准去定价，大家常见的都是以年龄，比如成人正畸和青少年正畸，来大致区分治疗的差异。正畸收费的特点就是技术占主导，而不是结果。不同资历的医生收费的差异会高达数十倍。其实，很容易理解，技术活嘛，收费有差异，正常。在美国找正畸医生做正畸比找牙医做正畸一般是会贵一倍左右。这是有严格专科准入制度和非常标准的医生培养机制前提下的稳定差价。当然也会有很多收费差异非常大的个例。在国内，除了相同的问题，还有正畸市场的混乱，没有专科准入导致正畸水平天壤之别，甚至可以说从不要钱到几十万的收费都有。可以给你一些个人参考，大的方向来说这个世界上除了蓄意欺骗，绝大数都是一分钱一分货。面对这种情况我们如何去选择呢？但是金钱是有限的，任何东西都可以找到一个性价比高的拐点，这个需要非常熟悉市场的人来帮你；而对于正畸来说，小编建议首先需要找正畸专科医生来进行治疗，其次规范的医疗环境，对于健康是基本保证。其次朋友亲戚的口碑也是非常重要的，找到靠谱的医生，基本收费也不会偏离多少。牙齿每天的工作时间很长，如果是吃货那牙齿还是要加班的，所以在牙齿上投资要谨慎！更要明白牙齿自己的诉求，一味地看中一时的价格，可能会毁了整口牙。牙齿矫正是需要医生精心、精密、精细化设计及操作过程的项目，记住一定要选择专业的正畸机构！"
-    },
-    {
-      id: 1,
-      imgPath: 'https://thumbnail0.baidupcs.com/thumbnail/084a063efdc63f7441b7d2538547f65c?fid=1913150091-250528-823501868467663&time=1533459600&rt=sh&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-XYY7rZRUnhfxSDQE97BTCGMOJVM%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=5026423855656756338&dp-callid=0&size=c710_u400&quality=100&vuk=-&ft=video',
-      title: "冰瓷牙2",
-      summary: "美国专利技术 六大优势 让牙齿美出新高度",
-      detail: "美国专利技术 六大优势 让牙齿美出新高度\n轻松：患者无需磨牙，患者无痛无不适 。\n超薄：超薄可至0.1 - 0.2mm，无异物感，如牙齿的隐形眼镜。\n超硬：硬度可达500mpa，不着色，永久美白。\n快捷：1-2个小时即可完成全口修复\n美观：好莱坞美白色，通透靓丽，年轻健康。\n可逆：不需要时微波震荡即可卸除，原牙无损伤。"
-    },
-    {
-      id: 2,
-      imgPath: 'https://thumbnail0.baidupcs.com/thumbnail/6342b4676e8183f60d90f42e0edc3b2d?fid=1913150091-250528-837059148017529&time=1533459600&rt=sh&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-B714ra2KlmQ8dCSD6g2PX94IIt4%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=5026482203277711785&dp-callid=0&size=c710_u400&quality=100&vuk=-&ft=video',
-      title: "冰瓷牙3",
-      summary: "我是summary",
-      detail: "美国专利技术 六大优势 让牙齿美出新高度\n轻松：患者无需磨牙，患者无痛无不适 。\n超薄：超薄可至0.1 - 0.2mm，无异物感，如牙齿的隐形眼镜。\n超硬：硬度可达500mpa，不着色，永久美白。\n快捷：1-2个小时即可完成全口修复\n美观：好莱坞美白色，通透靓丽，年轻健康。\n可逆：不需要时微波震荡即可卸除，原牙无损伤。"
-    }
-    ],
+    }, ],
     currentItem: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
-      currentItem: this.data.listNews[options.id]
-    }),
+        currentItem: this.data.listNews[options.id]
+      }),
       wx.setNavigationBarTitle({
         title: this.data.currentItem.title
       })
@@ -45,49 +30,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
